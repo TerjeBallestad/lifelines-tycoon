d8b2592f47f0719299912d31d695fbdb6715ae4b
TerjeBallestad <terj.ba@gmail.com>
2026-05-18 21:40:55 +0200

    feat(event_bus): declare prototype signal set (9 events)

--- autoload/event_bus.gd
+++ autoload/event_bus.gd
@@ -1,1 +1,11 @@
 extends Node
+
+signal day_started(day: int)
+signal day_ended(day: int)
+signal overskudd_changed(client_id: StringName, value: float)
+signal caseworker_capacity_changed(current: float, max: float)
+signal case_file_updated(entry_id: StringName)
+signal diagnostic_completed(id: StringName)
+signal intervention_completed(id: StringName)
+signal action_failed(reason: StringName)
+signal tick(game_hours: float)
